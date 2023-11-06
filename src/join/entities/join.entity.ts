f86ac87da12e936972e433c333ff32302f2ea7c6
HeoChangwon <wonn22@gmail.com>
2023-11-06 13:57:21 +0900

    feat: 참여 엔터티 작성

--- src/join/entities/join.entity.ts
+++ src/join/entities/join.entity.ts
@@ -1,1 +1,35 @@
-export class Join {}
+import { Post } from 'src/post/entities/post.entity'
+import { User } from 'src/user/entities/user.entity'
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+
+@Entity('joins')
+export class Join extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: 'boolean', nullable: false, default: false })
+  canceled: boolean
+
+  @Column({
+    type: 'enum',
+    enum: ['pending', 'accepted', 'rejected'],
+    nullable: false,
+    default: 'pending',
+  })
+  status: string
+
+  @Column({ type: 'int', nullable: false, default: 0 })
+  matchingCount: number
+
+  @ManyToOne(() => User, (user) => user.joins)
+  user: User
+
+  @ManyToOne(() => Post, (post) => post.joins)
+  post: Post
+}
