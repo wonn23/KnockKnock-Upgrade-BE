f78f1d0699a9a561fab0d9469a85ef41f0384393
HeoChangwon <wonn22@gmail.com>
2023-11-06 13:56:46 +0900

    feat: 댓글 엔터티 작성

--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,1 +1,24 @@
-export class Comment {}
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+import { User } from 'src/user/entities/user.entity'
+import { Post } from '../../post/entities/post.entity'
+
+@Entity('comments')
+export class Comment extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: 'varchar', nullable: false })
+  content: string
+
+  @ManyToOne(() => Post, (post) => post.comments)
+  post: Post
+
+  @ManyToOne(() => User, (user) => user.comments)
+  user: User
+}
